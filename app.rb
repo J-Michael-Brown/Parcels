744298d142a8c806a6808ff9cc2ee1037bcc4e1d
Michael Brown and Ralph Perdomo <ralph@>
2018-11-26 11:19:24 -0800

    reach mvp

--- app.rb
+++ app.rb
@@ -5,7 +5,19 @@ require ('./lib/parcels.rb')
 require ('pry')
 
 get ("/") do
-  @test= "i work."
+
   erb (:index)
+
+end
+get ("/box") do
+
+  user_length = params.fetch("length").to_i
+  user_width = params.fetch("width").to_i
+  user_height = params.fetch("height").to_i
+  user_weight = params.fetch("weight").to_i
+  user_speed = params.fetch("speed")
+  box = Parcel.new(user_length,user_width,user_height,user_weight)
+  @cost = box.cost_to_ship(user_speed)
+  erb (:output)
 
 end
